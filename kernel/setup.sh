509aa3eeadeb28495d5bbf0807eab3d63290db36
Reina in RainyLand <rainyxeon@gmail.com>
2025-07-22 20:11:07 +0700

    fix: custom setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -38,8 +38,9 @@ perform_cleanup() {
 
 # Sets up or update KernelSU-Next environment
 setup_kernelsu() {
-    echo "[+] Setting up KernelSU-Next..."
+    echo "[+] Setting up KernelSU-Next-Custom..."
     test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/kernel-build-from-rainyland/KernelSU-Next-Custom && echo "[+] Repository cloned."
+    mv ./KernelSU-Next-Custom ./KernelSU-Next
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
