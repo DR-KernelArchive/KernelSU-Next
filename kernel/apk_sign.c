103ed687269d739f5ba0594fcf91d6d09df4fcb3
Renia in RainyLand <rainyxeon@gmail.com>
2025-11-10 16:55:51 +0700

    add: deepinrain ksu (ksun legacy) hash

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -345,5 +345,6 @@ bool ksu_is_manager_apk(char *path)
 		|| check_v2_signature(path, 0x033b, "c371061b19d8c7d7d6133c6a9bafe198fa944e50c1b31c9d8daa8d7f1fc2d2d6")  // KernelSU
 		|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")     // MKSU
 		|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")   // KOWSU
+		|| check_v2_signature(path, 0x2e8, "33bd5db5d1263ecac9c41e9015909b52e87eb58527ba8059b38ffe5d1e188716")   // DeepinRain KSU
 		|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b");   // RKSU
 }
